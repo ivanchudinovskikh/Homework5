ce27cd8b2f838444d516e1a7994d49b60a81b463
Ivan <johntsuchi@gmail.com>
2023-11-14 20:34:42 +0400

    Initial Commit

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class House {
     public static void main(String[] args) {
         HashMap<Integer, String> people = new HashMap<>();
-        ArrayList<String> people18plus = new ArrayList<String>();
+        ArrayList<String> people18plus = new ArrayList<>();
         people.put(13, "Патриарх Матье Пар");
         people.put(22, "Петрович Петя Петькович");
         people.put(55, "Анджелика Жали Ликова");
@@ -12,12 +12,12 @@ public class House {
         people.put(26, "Allah Sultan Magamedovich");
 
         for (Map.Entry<Integer, String> entry : people.entrySet()) {
-            if (entry.getKey() > 18) {
+            if (entry.getKey() >= 18) {
                 people18plus.add(entry.getValue());
             }
         }
         Collections.sort(people18plus);
-        System.out.println(people18plus.toString());
+        System.out.println(people18plus);
     }
 }
 
